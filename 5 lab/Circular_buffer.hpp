67b798ad9b80db91c7ad0ec20fb148de35ed6e1a
georgesh08 <georgiyshulyak@gmail.com>
2021-05-16 23:03:20 +0300

    some fixes 1

--- 5 lab/Circular_buffer.hpp
+++ 5 lab/Circular_buffer.hpp
@@ -1,66 +1,105 @@
 #ifndef INC_5_LAB_CIRCULAR_BUFFER_HPP
 #define INC_5_LAB_CIRCULAR_BUFFER_HPP
+#include <stdexcept>
 
 template<typename T>
-class Circular_buffer{
+class Circular_buffer {
 public:
-    explicit Circular_buffer(int size){
+    explicit Circular_buffer(int size) {
         array = new int[size];
         capacity = size;
-        head = array;
+        head = tail = array;
         tail = array;
         content_size = 0;
     }
 
-    bool is_full(){
-        return capacity == tail-head;
+    bool is_full() {
+        return capacity == content_size;
     }
 
-    void increment_tail(){
+    void increment_tail() {
         ++tail;
-        ++content_size;
+        if (capacity != content_size)
+            ++content_size;
     }
 
-    int get_content_size(){
-        return (tail - head);
+    T &operator[](const int index){
+        return *(array + index);
     }
 
-    int get_capacity(){
-        return this->capacity;
+    int get_content_size() const {
+        return content_size;
     }
 
-    void increment_head(){
-        if(content_size == 0)
+    int get_capacity() const {
+        return capacity;
+    }
+
+    bool is_empty() const {
+        return content_size == 0;
+    }
+
+    void increment_head() {
+        if (content_size == 0)
             return;
         ++head;
-        --content_size;
     }
 
-    void push_back(const T& item){
-        if(is_full()) {
-            *head = item;
-            tail = head;
-            increment_head();
-        }
-        else {
+    T* begin(){
+        return array;
+    }
+
+    T* end(){
+        if(content_size < capacity)
+            return tail;
+        return array+capacity;
+    }
+
+    void push_back(const T &item) {
+        if (is_full()) {
+            if(head == end()-1){
+                tail = head;
+                *tail = item;
+                head = begin();
+            }
+            else {
+                tail = head;
+                *tail = item;
+                increment_head();
+            }
+        } else {
             *tail = item;
             increment_tail();
         }
     }
 
-    void print(){
-        for(int i = 0; i < content_size; ++i){
+    void pop_back(){
+        if(content_size == 0)
+            throw std::runtime_error("Buffer is empty");
+        else{
+            if(tail == begin())
+                tail = end()-1;
+            else if(tail == end()){
+                tail -= 2;
+            }
+            else
+                --tail;
+        }
+        --content_size;
+    }
+
+    void print() {
+        for (int i = 0; i < content_size; ++i) {
             std::cout << array[i] << " ";
         }
     }
 
-    Circular_buffer &operator[](int i){
-    }
 private:
-    T *head;
-    T *tail;
+    T* head;
+    T* tail;
     int capacity;
     int content_size;
-    T *array;
+    T* array;
 };
+
 #endif
--- 5 lab/main.cpp
+++ 5 lab/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <algorithm>
 #include "Circular_buffer.hpp"
 
 int main(){
-    Circular_buffer<int> cb(5);
+    Circular_buffer<int> cb(3);
+    cb.push_back(12);
+    cb.push_back(2);
+    cb.push_back(9);
+    cb.pop_back();
+    cb.print();
     return 0;
 }
