7882fb89fe75146368dca694c64216171d40b80c
georgesh08 <georgiyshulyak@gmail.com>
2021-05-20 18:22:49 +0300

    some fixes 3

--- 5 lab/Circular_buffer.hpp
+++ 5 lab/Circular_buffer.hpp
@@ -1,105 +1,112 @@
 #ifndef INC_5_LAB_CIRCULAR_BUFFER_HPP
 #define INC_5_LAB_CIRCULAR_BUFFER_HPP
 #include <stdexcept>
+#include <cstdlib>
 
 template<typename T>
 class Circular_buffer {
 public:
-    explicit Circular_buffer(int size) {
-        array = new int[size];
-        capacity = size;
-        head = tail = array;
-        tail = array;
-        content_size = 0;
+    explicit Circular_buffer(int buf_size) {
+        buffer_ = new T[buf_size];
+        capacity = buf_size;
+        head = tail = buffer_;
+        buf_end = buffer_ + capacity;
+        buf_begin = buffer_;
+        size = 0;
     }
 
-    bool is_full() {
-        return capacity == content_size;
+    template<class Pointer>
+    Pointer add(Pointer ptr, int n) const {
+        return ptr + ((n + ptr) < buf_end ? n : n - capacity);
     }
 
-    void increment_tail() {
-        ++tail;
-        if (capacity != content_size)
-            ++content_size;
+    template<class Pointer>
+    void increment(Pointer &ptr) const {
+        if (++ptr == buf_end)
+            ptr = buffer_;
     }
 
-    T &operator[](const int index){
-        return *(array + index);
+    template<class Pointer>
+    void decrement(Pointer &ptr) const {
+        if (ptr == buffer_)
+            ptr = buf_end;
+        --ptr;
     }
 
-    int get_content_size() const {
-        return content_size;
+    bool full() const { return capacity == size; }
+
+    bool empty() const { return size == 0; }
+
+    T &operator[](int index) {
+        if (index < 0 || index > capacity - 1)
+            throw std::runtime_error("Index out of range.");
+        return *add(head, index);
     }
 
-    int get_capacity() const {
-        return capacity;
+    int get_size() const { return size; }
+
+    int get_capacity() const { return capacity; }
+
+    void replace(T *pos, T item) {
+        *pos = item;
     }
 
-    bool is_empty() const {
-        return content_size == 0;
+    T *begin() { return buf_begin; }
+
+    T *end() {
+        if (full())
+            return buf_end;
+        return tail;
     }
 
-    void increment_head() {
-        if (content_size == 0)
-            return;
-        ++head;
-    }
-
-    T* begin(){
-        return array;
-    }
-
-    T* end(){
-        if(content_size < capacity)
-            return tail;
-        return array+capacity;
-    }
-
-    void push_back(const T &item) {
-        if (is_full()) {
-            if(head == end()-1){
-                tail = head;
-                *tail = item;
-                head = begin();
-            }
-            else {
-                tail = head;
-                *tail = item;
-                increment_head();
-            }
+    void push_back(const T item) {
+        if (full()) {
+            if (empty())
+                return;
+            replace(tail, item);
+            increment(tail);
+            head = tail;
         } else {
-            *tail = item;
-            increment_tail();
+            replace(tail, item);
+            increment(tail);
+            ++size;
         }
     }
 
-    void pop_back(){
-        if(content_size == 0)
-            throw std::runtime_error("Buffer is empty");
-        else{
-            if(tail == begin())
-                tail = end()-1;
-            else if(tail == end()){
-                tail -= 2;
-            }
+    void pop_back() {
+        if (size == 0)
+            throw std::runtime_error("Buffer is empty.");
+        else {
+            if (tail == begin())
+                tail = end() - 1;
             else
                 --tail;
         }
-        --content_size;
+        --size;
     }
 
-    void print() {
-        for (int i = 0; i < content_size; ++i) {
-            std::cout << array[i] << " ";
+    void push_front() {
+        if (head == begin()) {
+
+        }
+    }
+
+    void resize(const int new_size) {
+        capacity = new_size;
+        T *tmp = new T[new_size];
+        if (new_size > capacity) {
+
         }
     }
 
 private:
-    T* head;
-    T* tail;
+    T *head;
+    T *tail;
+    const T *buf_end;
+    const T *buf_begin;
     int capacity;
-    int content_size;
-    T* array;
+    int size;
+    T *buffer_;
 };
 
 #endif
--- 5 lab/main.cpp
+++ 5 lab/main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 #include "Circular_buffer.hpp"
 
-int main(){
+int main() {
     Circular_buffer<int> cb(3);
-    cb.push_back(12);
+    cb.push_back(1);
     cb.push_back(2);
-    cb.push_back(9);
+    cb.push_back(3);
+    for (int i = 0; i < 3; ++i) {
+        std::cout << cb[i] << " ";
+    }
+    cb.push_back(4);
+    cb.push_back(5);
+    std::cout << "\n";
+    for (int i = 0; i < 3; ++i) {
+        std::cout << cb[i] << " ";
+    }
+/*    std::cout << "Buffer:\n";
+    std::cout << cb.get_capacity() << " " << sizeof(Circular_buffer<int>) + cb.get_content_size()*sizeof(int) << " " << cb.get_content_size() << "\n";
     cb.pop_back();
-    cb.print();
+    std::cout << cb.get_capacity() << " " << sizeof(Circular_buffer<int>) + cb.get_content_size()*sizeof(int) << " " << cb.get_content_size() << "\n";
+    std::vector<int> tmp(3);
+    for (int i = 0; i < tmp.size(); ++i) {
+        tmp[i] = i + 1;
+    }
+    std::cout << "Vector:\n";
+    std::cout << tmp.capacity() << " " << tmp.size() * sizeof(int) + sizeof(std::vector<int>) << " " << tmp.size()
+              << "\n";
+    tmp.pop_back();
+    std::cout << tmp.capacity() << " " << tmp.size() * sizeof(int) + sizeof(std::vector<int>) << " " << tmp.size()
+              << "\n";*/
     return 0;
 }
